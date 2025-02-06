17686ed35c7640723f97755a1f101ab6ce4bb68b
Manoel Lima <manoelsslima@yahoo.com.br>
2025-02-06 01:10:18 -0400

    feat: challenge solution

--- src/index.js
+++ src/index.js
@@ -5,26 +5,69 @@ import "./style.css";
 function App() {
   return (
     <div className="card">
-      <Avatar />
+      <Avatar imageName="profile.jpeg" />
       <div className="data">
-        <Intro />
-        <SkillList />
+        <Intro
+          name="Manoel Lima"
+          bio="Software Engineer with over 13 years of experience
+          specializing in Java, REST APIs, and Spring Boot."
+        />
+        <SkillList
+          skills={[
+            { tech: "java", color: "#0aF", emoji: "0x1F4AA" },
+            { tech: "spring boot", color: "#0F0", emoji: "0x1F4AA" },
+            { tech: "AWS", color: "#F00", emoji: "0x1F466" },
+            { tech: "React", color: "#FF0", emoji: "0x1F476" },
+            { tech: "PostgreSQL", color: "#0FF", emoji: "0x1F4AA" },
+          ]}
+        />
       </div>
     </div>
   );
 }
 
-function Avatar() {}
+function Avatar(props) {
+  return (
+    <div className="avatar">
+      <img src={props.imageName} alt={props.imageName} />
+    </div>
+  );
+}
 
-function Intro() {}
+function Intro(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <div>{props.bio}</div>
+    </div>
+  );
+}
 
 /* should have one Skill component for each
 web dev skill that you have, customized
 with props */
-function SkillList() {}
+function SkillList(props) {
+  const skillList = props.skills;
+  const skill_result = [];
+  for (let index = 0; index < skillList.length; index++) {
+    const tech = skillList[index].tech;
+    const color = skillList[index].color;
+    const emoji = skillList[index].emoji;
+    skill_result.push(
+      <Skill tech={tech} color={color} emoji={emoji} key={index} />
+    );
+  }
+  return <div className="skill-list">{skill_result}</div>;
+}
 
 // each skill must have a name, a color and an emoji
-function Skill() {}
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      {props.tech} {String.fromCodePoint(props.emoji)}
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
